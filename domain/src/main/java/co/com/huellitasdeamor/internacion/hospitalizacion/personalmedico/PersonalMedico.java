bb892acda115e7a412e799e8fbd6f6e5856c5b1b
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-18 22:16:25 -0500

    Agregando comportamiento
    Se  agregan los comportamientos en la entidad agregada ÂÂpersonalMedico

--- domain/src/main/java/co/com/huellitasdeamor/internacion/hospitalizacion/personalmedico/PersonalMedico.java
+++ domain/src/main/java/co/com/huellitasdeamor/internacion/hospitalizacion/personalmedico/PersonalMedico.java
@@ -1,10 +1,10 @@
 package co.com.huellitasdeamor.internacion.hospitalizacion.personalmedico;
 
 import co.com.huellitasdeamor.internacion.hospitalizacion.personalmedico.events.PersonalMedicoCreado;
-import co.com.huellitasdeamor.internacion.hospitalizacion.personalmedico.valueobject.Horrario;
-import co.com.huellitasdeamor.internacion.hospitalizacion.personalmedico.valueobject.PersonalMedicoID;
+import co.com.huellitasdeamor.internacion.hospitalizacion.personalmedico.valueobject.*;
 import co.com.sofka.domain.generic.AggregateEvent;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class PersonalMedico extends AggregateEvent<PersonalMedicoID> {
@@ -17,6 +17,50 @@ public class PersonalMedico extends AggregateEvent<PersonalMedicoID> {
         super(entityId);
         appendChange(new PersonalMedicoCreado(horrario)).apply();
     }
+
+    //Agregar Auxiliar veterinario
+    public void agregarAuxiliar(AuxiliarVeterinarioID entityId, Nombre nombre, Funcion funcion){
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(nombre);
+        Objects.requireNonNull(funcion);
+        appendChange(new AuxiliarAgregado(entityId,nombre,funcion)).apply();
+    }
+
+    //Agregar Veterinario
+    public void agregarVeterinario(VeterinarioID entityId, Nombre nombre, Especialidad especialidad){
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(nombre);
+        Objects.requireNonNull(especialidad);
+        appendChange(new VeterinarioAgregado(entityId,nombre,especialidad));
+    }
+
+    //Cambiar Horrario del personal medico
+    public void cambiarHorrario(Horrario horrario){
+        Objects.requireNonNull(horrario);
+        appendChange(new HorrarioCambiado(horrario)).apply();
+
+    }
+
+    //Actualizar Veterinario
+    public void actualizarVeterinario(VeterinarioID entityId, Nombre nombre, Especialidad especialidad){
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(nombre);
+        Objects.requireNonNull(especialidad);
+        appendChange(new VeterinarioActualizado(entityId,nombre,especialidad));
+    }
+
+    //Actualizar funcion de auxiliar veterinario
+    public void actualizarFuncioDeUnAuxiliar(AuxiliarVeterinarioID entityId,Funcion funcion){
+        appendChange(new FuncionDeAuxiliarAlctualizado(entityId,funcion)).apply();
+    }
+
+    //Eliminar Auxiliar
+    public void eliminarAuxiliarVeterinario(AuxiliarVeterinarioID entityId){
+        Objects.requireNonNull(entityId);
+        appendChange(new AuxiliarVEterinarioEliminado(entityId)).apply();
+    }
+
+
 
 
 

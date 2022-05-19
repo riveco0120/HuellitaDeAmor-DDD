cc33be40d2fdf976bed3ec3d10c61f59184c026d
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-19 13:29:17 -0500

    Crando test y useCaso
    Se implementa el caso de uso pára actualizar nombre del dueño y su test

---
+++ business/src/main/java/co/com/huellitasdeamor/internacion/hospitalizacion/mascota/commands/ActualizarNombreDueñoUseCase.java
@@ -0,0 +1,17 @@
+package co.com.huellitasdeamor.internacion.hospitalizacion.mascota.commands;
+
+import co.com.huellitasdeamor.internacion.hospitalizacion.mascota.Mascota;
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+
+public class ActualizarNombreDueñoUseCase extends UseCase<RequestCommand<ActualizarNombreDueño>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<ActualizarNombreDueño> actualizarNombreDueñoRequestCommand) {
+        var command = actualizarNombreDueñoRequestCommand.getCommand();
+        var mascota = Mascota.from(command.getMascotaID(),retrieveEvents(command.getMascotaID().value()));
+        mascota.actualizarNombreDueño(command.getNombreDueño());
+        emit().onResponse(new ResponseEvents(mascota.getUncommittedChanges()));
+    }
+}

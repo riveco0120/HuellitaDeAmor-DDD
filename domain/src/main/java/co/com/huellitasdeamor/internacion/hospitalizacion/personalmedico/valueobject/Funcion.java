56f3102e0fb60ead5c3ce2473b3694a44d824be9
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-19 23:35:15 -0500

    Validando objetos de valor
    Se validaron los objetos de valor de agregado persona medico

--- domain/src/main/java/co/com/huellitasdeamor/internacion/hospitalizacion/personalmedico/valueobject/Funcion.java
+++ domain/src/main/java/co/com/huellitasdeamor/internacion/hospitalizacion/personalmedico/valueobject/Funcion.java
@@ -9,11 +9,30 @@ public class Funcion implements ValueObject<String> {
     private final String value;
 
     public Funcion(String value){
-        this.value= Objects.requireNonNull(value);
+        this.value = Objects.requireNonNull(value);
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("Debe Contener almenos una funcion");
+        }
+        if (this.value.length() <= 2) {
+            throw new IllegalArgumentException("La funcion debe tener mas de dos caracteres");
+        }
     }
 
     @Override
     public String value() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object prueba) {
+        if (this == prueba) return true;
+        if (prueba == null || getClass() != prueba.getClass()) return false;
+        Funcion that = (Funcion) prueba;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
